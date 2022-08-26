da6357de64d659ae23aeb5f0e522d81e2b10893f
david <david.balzan@gmail.com>
2022-08-26 10:41:30 +0200

    swap boxes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,15 +4,20 @@ import { useEffect, useRef } from "react";
 
 const Home: NextPage = () => {
   const boxRef: any = useRef();
+  const boxRef2: any = useRef();
 
   useEffect(() => {
-    gsap.to(boxRef.current, { y: "+=360" });
+    gsap.to(boxRef.current, { y: "+=100" });
+    gsap.to(boxRef2.current, { y: "-=100" });
   });
   return (
     <>
       <div className="app">
         <div className="box" ref={boxRef}>
           Hello
+        </div>
+        <div className="box" ref={boxRef2}>
+          There
         </div>
       </div>
     </>
